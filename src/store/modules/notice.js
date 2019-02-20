5e39d019b82e0e7ef3a3b305d7ce0712048753c2
Jae Mok <ashnamuh@gmail.com>
2019-02-20 21:22:30 +0900

    implement notices list with infinite scroll

--- src/store/modules/notice.js
+++ src/store/modules/notice.js
@@ -1,8 +1,34 @@
+import axios from 'axios'
 
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {}
+  state: {
+    notices: [],
+    accumulatedLength: 0
+  },
+  mutations: {
+    INSERT_NOTICES (state, notices) {
+      console.log('INSERT_NOTICES')
+      state.notices = state.notices.concat(notices)
+    },
+    UPDATE_LENGTH (state, newLength) {
+      console.log('UPDATE_LENGTH')
+      state.accumulatedLength += newLength
+    }
+  },
+  actions: {
+    async fetch (context, query) {
+      console.log(query)
+      const page = query.page || 1
+      const order = query.order || 'desc'
+      const category = query.category
+      const { data } = await axios.get(`http://comento.cafe24.com/request.php?page=${page}&ord=${order}&category=${category}`)
+      context.commit('INSERT_NOTICES', data.list)
+      context.commit('UPDATE_LENGTH', data.list.length)
+    }
+  },
+  getters: {
+    getNotices: state => () => state.notices,
+    getAccumulatedLength: state => () => state.accumulatedLength
+  }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li class="notice" v-for="notice in notices" :key="notice.no">
+        title: {{ notice.title }}
+        no: {{ notice.no }}
+      </li>
+    </ul>
+    <div v-infinite-scroll="loadMore"
+      infinite-scroll-distance="50"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   },
-  created () {
-    console.log(this.$store)
+  data () {
+    return {
+      page: 1
+    }
+  },
+  computed: {
+    notices () {
+      return this.$store.getters['notice/getNotices']()
+    },
+    accumulatedLength () {
+      return this.$store.getters['notice/getAccumulatedLength']()
+    }
+  },
+  async created () {
+  },
+  methods: {
+    async loadMore () {
+      await this.$store.dispatch('notice/fetch', {order: 'desc', category: 1, page: this.page})
+      this.page++
+    }
   }
 }
 </script>
+
+<style scoped lang="less">
+.notice {
+  margin-top: 20px;
+  height: 200px;
+  background-color: #ffff00;
+}
+</style>
